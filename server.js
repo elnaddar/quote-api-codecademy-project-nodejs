df4aeff55d34192d33cbd51d4ddaa373c95b2dab
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-06 03:08:14 +0200

    created quotes router.

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
+const quotesRouter = express.Router();
 
 app.use(express.static('public'));
+
+app.use('/api/quotes',quotesRouter);
 
 app.listen(PORT, ()=>{
     console.log("listening to the server...");
